ede2bfebeea7845ab4e240ec063219b87694d7ce
momka45 <milromchuk@gmail.com>
2017-12-29 20:04:10 +0200

    Added getLocation and addEvent

--- app/src/main/java/ucu/in/ua/mapstest/MainActivity.java
+++ app/src/main/java/ucu/in/ua/mapstest/MainActivity.java
@@ -3,6 +3,7 @@ package ucu.in.ua.mapstest;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
@@ -16,6 +17,16 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Date;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener, OnMapReadyCallback{
     MapFragment fragMap;
@@ -75,11 +86,41 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     @Override
     public void onMapReady(GoogleMap map) {
+        LatLng position = Lviv;
+        getLocation mGetLocation = new getLocation();
+        mGetLocation.execute("Львів");
+        try {
+            position = mGetLocation.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(position,13));
+        addEventMarker(new Event(1,"Виставкa \"Скарби династії Цін\"",new Date(0),"Палаццо Бандінеллі, пл. Ринок 2","Some description ---"),map);
+        addEventMarker(new Event(2,"Виставка \"Сучасне польське мистецтво\"",new Date(0),"Музей скульптури І. Г. Пінзеля, пл. Митна, 2","Some description ---"),map);
+        addEventMarker(new Event(3,"Репертуар Театру Лесі на грудень",new Date(0),"Театр імені Лесі Українки, вул. Городоцька, 36","Some description ---"),map);
+        addEventMarker(new Event(4,"Репертуар Львівського театру ляльок на грудень",new Date(0),"Львівський театр ляльок, площа Данила Галицького, 1","Some description ---"),map);
+        addEventMarker(new Event(6,"Фестиваль \"Святкуємо особливе Різдво у Львові\"",new Date(0),"Львів, площа Ринок, площа перед Львівською оперою","Some description ---"),map);
+        addEventMarker(new Event(7,"Виставка \"Народний дереворит у художніх збірках Львова\"",new Date(0),"Музей етнографії та художнього промислу, пр. Свободи,15","Some description ---"),map);
+        addEventMarker(new Event(8,"Виставка «Археологія автострад. Розкопки під час масштабного будівництва доріг біля Кракова»",new Date(0),"Музей етнографії та художнього промислу, пр. Свободи,15","Some description ---"),map);
+        addEventMarker(new Event(9,"Палац Потоцьких — Зимова резиденція Львова",new Date(0),"Палац Потоцьких, вул. Коперника, 15","Some description ---"),map);
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Lviv,10));
+    }
+
+    public void addEventMarker(Event event,GoogleMap map) {
         Log.v(TAG,"I created marker");
-        map.addMarker(new MarkerOptions().position(Lviv).title("Lviv"));
-
+        LatLng position = Lviv;
+        getLocation mGetLocation = new getLocation();
+        mGetLocation.execute(event.getPlace());
+        try {
+            position = mGetLocation.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        map.addMarker(new MarkerOptions().position(position).title(event.getName()));
     }
 
     @Override
@@ -92,4 +133,46 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         mFragmentTransaction.commit();
         super.onPause();
     }
+
+    class getLocation extends AsyncTask<String,Void,LatLng> {
+
+        @Override
+        protected LatLng doInBackground(String... sites) {
+            double lng = 0, lat = 0;
+            for (String site: sites) {
+                Log.v(TAG,"Start async");
+                String json = "";
+                for (int i = 0; i < site.length(); i++) {
+                    if(site.charAt(i) == ' '){
+                        site = site.replace(' ','+');
+                    }
+                }
+                try {
+                    URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=");
+                    String key = "&key=AIzaSyAuurSdVgrVBkMnDG7Z4Dur0TT5ZRY44-k";
+                    url = new URL(url.toString()+site+key);
+                    Log.v(TAG,"try to connect");
+                    Scanner sc = new Scanner(url.openConnection().getInputStream());
+                    Log.v(TAG,"connected");
+                    while(sc.hasNextLine()) {
+                        json += sc.nextLine() + '\n';
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                JSONObject place = null;
+                try {
+                    place = new JSONObject(json);
+                    lng = place.getJSONArray("results").optJSONObject(0).getJSONObject("geometry").getJSONObject("location").getDouble("lng");
+                    lat = place.getJSONArray("results").optJSONObject(0).getJSONObject("geometry").getJSONObject("location").getDouble("lat");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return new LatLng(lat,lng);
+        }
+    }
 }
+
